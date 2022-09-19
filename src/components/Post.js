181bb0c9bd6f3a28022d7ce6affc939ba60f514a
AliMohammad420 <aliwessam509@gmail.com>
2022-09-19 15:54:06 +0000

    lab-07

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,40 +1,58 @@
 import axios from "axios";
-import {useEffect} from "react";
-import {useState} from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import AddCommentForm from "./Add-comment-form";
 import React from 'react';
 
-
-function Post (props) {
-    const [post, setPost] = useState([]);
+function Post ( props ) {
+    const [ post, setPost ] = useState( [] );
     const getData = async () => {
-        const dataPost = await axios.get( `https://post-401.herokuapp.com/post`);
-        setPost(dataPost.data.post);
+        const response = await axios.get( `${process.env.REACT_APP_HEROKU_URL}/post` );
+        setPost( response.data.posts );
     };
+
+
+    const handleDelete = async ( id ) => {
+        await axios.delete( `${process.env.REACT_APP_HEROKU_URL}/post/${id}` );
+        getData();
+    };
+
 
     useEffect( () => {
         getData();
-    }, [props.rerender]);
+    }, [props.rerender] );
     return (
         <>
-            {post && post.map((post, idx) => {
+            {post && post.map( ( post, idx ) => {
                 return (
-                    <div className="post-class" style={{ justifyContent: 'center', margin: '1rem' }} key={idx}>
-                     {post.Comments &&
-                     <h2>Comments</h2>
-                        }
-                    {post.Comments && post.Comments.map((comment, idx) => {
-                     return (
-                        <div className="card" style={{ justifyContent: 'center', margin: '1rem' }} key={idx}>
-                            <div className="card-body">
-                               <p className="card-text">{comment.content}</p>
-                                </div>
-                        </div>
+                    <div className="text">
+                         <div key={idx}>
+                        <div className="card-body">
+                            <h1 className="card-title">{post.title}</h1>
+                            <p className="card-text">{post.content}</p>
+                    </div>
+                        <div>
+                            {post.Comments && <h2>Comments</h2>}
+                            {post.Comments && post.Comments.map( ( comment, idx ) => {
+                                return (
+                                    <div className="card" style={{ justifyContent: 'center', margin: '1rem' }} key={idx}>
+                                        <div className="card-body">
+                                            <p className="card-text">{comment.content}</p>
+                                        </div>
+                                    </div>
                                 );
                             }
                             )}
-                            <AddCommentForm postID={post.id} getPost={getData} />
+                              </div>
+                              <AddCommentForm postId={post.id} getData={getData} />
+                            <div>
+                            <button  onClick={() => {
+                                handleDelete( post.id );
+                            }}>delete post</button>
                         </div>
+
+                        </div>
+                    </div>
                 );
             }
             )}
