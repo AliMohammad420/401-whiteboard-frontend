9d123258ea49d25fe7c09eac068013d6aa58bdec
AliMohammad420 <aliwessam509@gmail.com>
2022-10-12 15:35:25 +0000

    reducers

---
+++ src/actions/postActions.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { actionType } from "../config/constant";
+
+export const getPost = ( dispatch ) => {
+    axios.get( `${process.env.REACT_APP_HEROKU_URL}/post` ).then( ( res ) => {
+        dispatch( { type: actionType.GET_POSTS, payload: res.data } );
+    } ).catch( ( err ) => {
+        dispatch( { type: actionType.POST_ERROR, payload: err } );
+    } );
+};
+
